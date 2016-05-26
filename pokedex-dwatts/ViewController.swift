ab79ae88b21738339bd1360ebd3c59b56e7262e2
Damien Ahmad Watts <dwatts4@damiens-macbook-air.local>
2016-05-26 11:13:43 -0500

    Initial Commit

---
+++ pokedex-dwatts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-dwatts
+//
+//  Created by Damiens Macbook HO! on 5/26/16.
+//  Copyright © 2016 damien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
